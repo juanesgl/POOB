386b19fd1282e754722ad518cb5914f4f06b6d4a
Daniel Ruiz <edgar.ruiz@mail.escuelaing.edu.co>
2025-04-26 20:17:51 -0500

    Update main.java

--- Laboratorios/Laboratorio5/DMaxwell/src/main.java
+++ Laboratorios/Laboratorio5/DMaxwell/src/main.java
@@ -5,8 +5,8 @@ import presentacion.DMaxwellGUI;
 public class main {
 
     public static void main(String[] args) {
-            int particulasRojas = 15;
-            int particulasAzules = 1;
+            int particulasRojas = 20;
+            int particulasAzules = 10;
         // Crear una instancia de la GUI y hacerla visible
             SwingUtilities.invokeLater(() -> {
                 DMaxwellGUI frame = new DMaxwellGUI(particulasRojas, particulasAzules);
